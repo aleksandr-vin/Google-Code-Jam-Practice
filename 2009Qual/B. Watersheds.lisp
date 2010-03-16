6819ae9337cefdd780e06f59ef7855aa631b4981
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2010-03-16 03:37:32 +0300

    Done with the task. It runs rather fast, in comparison to Alien Language.

--- 2009Qual/B. Watersheds.lisp
+++ 2009Qual/B. Watersheds.lisp
@@ -2,15 +2,11 @@
 ;;;; B. Watersheds
 ;;;; Practicing for GCJ 2010, Aleksandr Vinokurov, 2010-03-10
 
+(defparameter *test*  t "Testing of the program")
 (defparameter *trace* nil "Tracing of the program")
 
 (defun trace! (&rest args)
   (when *trace* (apply #'format (cons t args))))
-
-(defclass geo-map ()
-  ((height :initarg :height)
-   (width  :initarg :width)
-   (data   :initarg :data))) ; 2D array of cells: (list elevation drainage)
 
 (defun elevation (cell)
   (car cell))
@@ -25,40 +21,43 @@
   (cons (elevation cell) (list (incf *sink-counter*))))
 
 (defun link (from-cell to-cell)
-  "Connect from-cells' drain-link to to-cells' drain"
+  "Returns a cell with from-cells' elevation and drain-link pointing
+   to to-cells' drain. Modifies from-cell."
   (setf (cadr from-cell)
-	(drain to-cell)))
-
-(defun neighboring-cells (data row col)
-  (list (aref data (1- row)     col)	; North
-	(aref data     row  (1- col))	; West
-	(aref data     row  (1+ col))	; East
-	(aref data (1+ row)     col)))	; South
+	(drain to-cell))
+  from-cell)
 
 (defun drainer (cell neighbours)
   "Return one element from neighbours to whom the cell drains.
    Or a sink if there are no one appropriate for it."
   (let ((lowest (first (stable-sort (remove (elevation cell) neighbours
-					    :key #'elevation :test-not #'>)
+					    :key #'elevation
+					    :test (complement #'>))
 				    #'< :key #'elevation))))
     (if lowest lowest (make-sink cell))))
 
+(defun connect-drains-for-cell (cell north west east south)
+  (link cell (drainer cell (list north west east south))))
+
+(defun connect-drains-on-a-parallel (par-1 par par+1)
+  (map 'list #'connect-drains-for-cell
+       (cdr par)			; Operation cell
+       (cdr par-1)			; North neighbour cell
+       par				; West neighbour cell
+       (cddr par)			; East neighbour cell
+       (cdr par+1)))			; South neighbour cell
+
 (defun connect-drains-on-map (geo-map)
-  "Connect cells' drainages by the rules from the task,
-   see http://code.google.com/codejam/contest/dashboard?c=90101#s=p1
-   Organizing them into directed acyclic graphs with sinks at their
-   ends. One DAG for a drainage basin.
-   Such DAG has one propery: it translates the sinks' label through
-   all its nodes"
-  (with-slots (height width data) geo-map
-    (loop for row from 1 to height
-       do (loop for col from 1 to width
-	     do (let ((cell (aref data row col)))
-		  (link cell
-			(drainer cell
-				 (neighboring-cells data row col)))
-		  (trace! "; ~a~%" data)))))
-  geo-map)
+  "Modify geo-map in a way that all cells that have 4 neighbours get their drains
+   connected to one of the neighbours or a sink. See task on Google Code Jam site
+   for details.
+   Returns new list of lists of cells that point to the modified original cells.
+   Border cells are not included, so each lists' length is twice decremented.
+   Such a map will be called unbordered-map."
+  (map 'list #'connect-drains-on-a-parallel
+       geo-map				; Northen parallel
+       (cdr geo-map)			; Operation parallel
+       (cddr geo-map)))			; Southen parallel
 
 (defun find-last-cons (y)
   (when (consp y)
@@ -66,63 +65,89 @@
 	(find-last-cons (car y)))))
 
 ;; Tests for FIND-LAST-CONS
-(format t ";; Testing FIND-LAST-CONST: ~a~%"
-	(and
-	 (eq    nil   (find-last-cons :a))
-	 (equal '(:a) (find-last-cons '(:a)))
-	 (equal '(:a) (find-last-cons '(((:a)))))))
+(if *test*
+    (format t ";; Testing FIND-LAST-CONST: ~a~%"
+	    (and
+	     (eq    nil   (find-last-cons :a))
+	     (equal '(:a) (find-last-cons '(:a)))
+	     (equal '(:a) (find-last-cons '(((:a))))))))
 
-(defparameter *last-used-char-code* nil "Counter of the char codes for labelling sinks")
+(defparameter *last-used-char-code* nil
+  "Counter of the char codes for labelling sinks.
+   Must be reset to the starting char-code before labelling a map.")
+
+(defun sink-label (last-cons)
+  "Returns label of the sink -- the last-cons actually point to the sink --
+   or generate a new label, if this sink has no label for now.
+   Modifies the sink."
+  (flet ((gen-sink-label ()
+	   (code-char (incf *last-used-char-code*))))
+    (if (numberp (car last-cons))
+	(setf (car last-cons) (gen-sink-label))
+	(car last-cons))))
 
 (defun basin-label (drainer)
   "Return label of the basin the drainer is member of.
-   If no label is provided for the basin, a new one is generated."
-  (flet ((gen-sink-label ()
-	   (code-char (incf *last-used-char-code*))))
-    (let ((last-cons (find-last-cons drainer)))
-      (if (numberp (car last-cons))
-	  (setf (car last-cons) (gen-sink-label))
-	  (car last-cons)))))
+   If no label is provided for the basin, a new one is generated.
+   Modifies the drainer."
+  (sink-label (find-last-cons drainer)))
 
-(defun label-map (geo-map)
+(defun cell-basin-label (cell)
+  (basin-label (drain cell)))
+
+(defun label-parallel (par)
+  (map 'list #'cell-basin-label par))
+
+(defun label-map (unbordered-map)
   "Label the maps' drainage basins with the characters alphabetically
-   from left to right, from top to bottom."
-  ;; Each map should have sinks labelled from #\a
-  (let ((*last-used-char-code* (1- (char-code #\a)))) 
-    (with-slots (height width data) geo-map
-      (loop for row from 1 to height
-	 do (loop for col from 1 to width
-	       do (let ((cell (aref data row col)))
-		    (setf (cdr cell) (basin-label (drain cell)))
-		    (trace! "; ~a~%" data))))))
-  geo-map)
+   from left to right, from top to bottom.
+   Labels start from #\a.
+   Modifies the drainers of the cells.
+   Returns a new map (list of lists)."
+  (let ((*last-used-char-code* (1- (char-code #\a))))
+    (map 'list #'label-parallel unbordered-map)))
 
-(defun to-string (geo-map)
- (with-slots (height width data) geo-map
-      (loop for row from 1 to height
-	 do (loop for col from 1 to width
-	       do (let ((basin (cdr (aref data row col))))
-		    (format t "~a " basin))))))
+(defmacro join-strings-with-char (ch a b)
+  `(concatenate 'string (string ,a) ,(string ch) (string ,b)))
 
-(defun read-next-map (stream)
+(defun join-strings-with-space (a b)
+  (join-strings-with-char #\Space a b))
+
+(defun join-strings-with-newline (a b)
+  (join-strings-with-char #\NewLine a b))
+
+(defun to-string (unbordered-map)
+  (reduce #'join-strings-with-newline
+	  (map 'list #'(lambda (x)
+			   (reduce #'join-strings-with-space x
+				   :from-end t))
+	       unbordered-map)
+	  :from-end t))
+
+
+(defun read-next-parallel (stream width heavens)
+  (concatenate 'list `(,heavens)
+	       (loop repeat width
+		  collect (cons (read stream) ; elevation
+				(list (gensym)))) ; cons cell for drain-link
+	       `(,heavens)))
+
+(defun read-next-map (stream &optional (border-elevation 10000))
   "Read a map from the stream.
    Two numbers H and W -- height and width -- are read from the first line.
    Then H lines follow with W numbers on each -- that is a map of cells' elevations.
-   Return an instance of geo-map where data is a 2D array of that map surrounded
-   with a border of cells with elevations of undoubted hight. Its size is (H+2)x(W+2)."
+   Return a list of lists that represent the map of cells. The first and last lists
+   are a North and South borders with the highest+1 elevations allowed. The first
+   and last elements of each sublist are West and East borders for the cells on
+   a parallel."
   (let* ((height (read stream))
 	 (width  (read stream))
-	 (data   (make-array (list (+ height 2) (+ width 2))
-			     :initial-element '(10000))))
-    (loop for row from 1 to height
-       do (loop for col from 1 to width
-	     do (setf (aref data row col)
-		      (list (read stream)       ; elevation
-			    (list (gensym)))))) ; cons cell for drain-link
-    (make-instance 'geo-map
-		   :height height
-		   :width  width
-		   :data   data)))
+	 (heavens `(,border-elevation))
+	 (parallel-heavens (make-list (+ width 2) :initial-element heavens)))
+    (concatenate 'list `(,parallel-heavens)
+		 (loop repeat height
+		    collect (read-next-parallel stream width heavens))
+		 `(,parallel-heavens))))
 
 (defun label-maps (&optional &key (in-stream *standard-input*)
 		   (out-stream *standard-output*))
@@ -134,11 +159,57 @@
      do (format out-stream "Case #~a:~%~a~%"
 		case (to-string (label-map (connect-drains-on-map map))))))
 
-(with-input-from-string (in-stream
-			 "1
+
+;; Program test
+(if *test*
+    (format t ";;; Program test: ~a~%"
+	    (string= "Case #1:
+a b b
+a a b
+a a a
+Case #2:
+a a a a a a a a a b
+Case #3:
+a a a
+b b b
+Case #4:
+a a a a a
+a a b b a
+a b b b a
+a b b b a
+a a a a a
+Case #5:
+a b c d e f g h i j k l m
+n o p q r s t u v w x y z
+"
+		     (with-output-to-string (out-stream)
+		       (with-input-from-string (in-stream "5
 3 3
 9 6 3
 5 9 6
-3 5 9")
-  (let ((*trace* t))
-    (label-maps :in-stream in-stream)))
+3 5 9
+1 10
+0 1 2 3 4 5 6 7 8 7
+2 3
+7 6 7
+7 6 7
+5 5
+1 2 3 4 5
+2 9 3 9 6
+3 3 0 8 7
+4 9 8 9 8
+5 6 7 8 9
+2 13
+8 8 8 8 8 8 8 8 8 8 8 8 8
+8 8 8 8 8 8 8 8 8 8 8 8 8
+")
+			 (label-maps :in-stream in-stream :out-stream out-stream))
+		       out-stream))))
+
+;; Interactive program function
+(defun run (in-file out-file)
+  "Run it from the shell with
+   ``alisp -L <fasl> -e '(run #p\"~/in.txt\" #p\"~/out.txt\")' -kill''"
+  (with-open-file (in in-file)
+    (with-open-file (out out-file :direction :output :if-exists :supersede)
+      (label-maps :in-stream in :out-stream out))))
