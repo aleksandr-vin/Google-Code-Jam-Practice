74a8508be778382c0a14a3f87fc39d5e62ccc06f
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2010-03-18 23:54:38 +0300

    Started reading the task C. Welcome to Code Jam

---
+++ 2009Qual/C. Welcome to Code Jam.lisp
@@ -0,0 +1,5 @@
+;;;; Google Code Jam -- Qualification Round 2009
+;;;; C. Welcome to Code Jam
+;;;; http://code.google.com/codejam/contest/dashboard?c=90101#s=p2
+;;;; Practicing for GCJ 2010, Aleksandr Vinokurov
+;;;; Started reading the task at 23:52, 2010-03-18
