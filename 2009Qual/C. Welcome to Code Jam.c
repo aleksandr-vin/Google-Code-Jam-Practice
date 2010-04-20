94f65d153e3670d7c233293f568ef5be4c5b9d2b
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2010-04-20 23:45:36 +0400

    C solution is not working

--- 2009Qual/C. Welcome to Code Jam.c
+++ 2009Qual/C. Welcome to Code Jam.c
@@ -10,6 +10,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
 unsigned int cps(const char * phrase,
 		 unsigned int phrase_len,
@@ -38,8 +39,8 @@ unsigned int cps(const char * phrase,
 	    spsc = 1;
 	  if (spsc > 0)
 	    {
-	      spsc += (rt[0] == ft && rp[0] != fp) ?
-		spsc : cps(phrase, phrase_len, rt, rt_len);
+	      spsc += /*(rt[0] == ft && rp[0] != fp) ?
+		spsc : */ cps(phrase, phrase_len, rt, rt_len);
 	    }
 	  return spsc;
 	}
@@ -61,43 +62,127 @@ void testcase1(void)
   const char         test_text[]   = "abbcc";
   const unsigned int test_cps      = 4;
 
-  printf("Test case #1: %s\n",
+  printf(";; Test case #1: %s\n",
 	 (test_cps == count_phrase_subsequencies(test_phrase, test_text) ?
 	  "ok." : "FAILED!"));
 }
 
+/**
+ *  Count PHRASE subsequencies on DATA
+ *
+ *  PHRASE is a zero-terminated string
+ *  DATA   is a series of CASES of zero-terminated strings
+ *  RESULT is a buffer for CASES results
+ */
 unsigned int *
 count_phrase_subsequencies_on_data(const char * phrase,
 				   const char * data,
 				   const unsigned int cases,
 				   unsigned int result[]) /* a buffer for results */
 {
+  unsigned int i = 0;
+  unsigned int itext_len = 0;
+  char *       itext = (char *)data;
+
+  for (i = 0; i < cases; i++)
+    {
+      result[i] = count_phrase_subsequencies(phrase, itext);
+      itext    += 1 + strlen(itext);
+    }
+  return result;
+}
+
+unsigned int * cps_on_cases(const char * phrase,
+			    const char * data,
+			    const unsigned int cases,
+			    unsigned int * result)
+{
+  const unsigned int newline_symb_len = strlen("\n");
+  unsigned int i       = 0;
+  char *       idata   = (char *)data;
+  char         text[501];
+
+  for(i = 0; i < 3; i++)
+    {
+      sscanf(idata, "%s", &text);
+      idata      += newline_symb_len + strlen(text);
+      result[i]   = count_phrase_subsequencies(phrase, text);
+    }
   return result;
 }
 
 /* A bit harder test case */
 void testcase2(void)
 {
+  const unsigned int newline_symb_len = strlen("\n");
   const char phrase[] = "abc";
-  const char data[]   = "\
-abbcc\0\
-aabbcc\0\
-abbbbaabbbbbaaaaabbdesssa\0";
+  const char data[]   = "3\n\
+abbcc\n\
+aabbcc\n\
+abbbbaabbbbbaaaaabbdesssa\n";
   const unsigned int  cps[3] = {4,8,0};
   unsigned int result_buf[3] = {0,0,0};
-  unsigned int *result       = NULL;
+  unsigned int * result      = NULL;
   unsigned int i             = 0;
   unsigned int test_result   = 1;
+  unsigned int cases = 0;
+  char cases_str[100];
+  char * idata = NULL;
 
-  result = count_phrase_subsequencies_on_data(phrase, data, 3, result_buf);
+  sscanf(data, "%s", &cases_str);
+  cases = (unsigned int)strtoul(cases_str, NULL, 10);
+  if (cases != 3)
+    {
+      printf("testcase #2 error: cases read are not equal to 3!\n");
+      return;
+    }
+  idata = (char *)data + newline_symb_len + strlen(cases_str);
+  result = cps_on_cases(phrase, idata, cases, result_buf);
   for(i = 0; i < 3; i++)
     test_result = test_result && (cps[i] == result[i]);
-  printf("Test case #2: %s\n",
+  printf(";; Test case #2: %s\n",
 	 (test_result ? "ok." : "FAILED!"));
 }
 
-int main(void)
+void run_on_file(const char *file)
 {
-  testcase1();
-  testcase2();
+  const unsigned int newline_symb_len = strlen("\n");
+  const char phrase[] = "welcome to code jam";
+  unsigned int cases  = 0;
+  unsigned int i      = 0;
+  char      text[501];
+  char cases_str[100];
+  FILE * stream;
+
+  stream = fopen(file, "r");
+  if (stream == NULL)
+    {
+      printf("testcase #3 error: can't open file %s: %s\n", file, strerror(errno));
+      return;
+    }
+
+  fgets(cases_str, 100, stream);
+  cases = (unsigned int)strtoul(cases_str, NULL, 10);
+
+  for (i = 0; i < cases; i++)
+    {
+      fgets(text, 501, stream);
+      text[strlen(text) - newline_symb_len] = '\0';
+      /*printf(";; text: %s\n", text);*/
+      printf("Case #%d: %04d\n", i + 1,
+	     count_phrase_subsequencies(phrase, text));
+    }
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc == 1)
+    {
+      testcase1();
+      testcase2();
+      return 0;
+    }
+
+  /*printf("; Running on file %s\n", argv[1]);*/
+  run_on_file(argv[1]);
 }
